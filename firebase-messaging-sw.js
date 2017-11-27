2aaebc74888907e418999411ced49835c0c63c9f
vladimir <logovazzik@gmail.com>
2017-11-28 02:26:21 +0300

    ##

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -33,6 +33,32 @@ self.addEventListener('notificationclick', function(event) {
     }));
 });
 
+var applyTemplate = function (template, replacements) {
+    return template.replace(/{(\w+)}/g, function (e, n) {
+        return undefined !== replacements[n] ? encodeURIComponent(replacements[n]) : "";
+    });
+};
+
+var getDateString =  function(dateString){
+    if(!dateString){
+        return '';
+    }
+    var dateTemplate = '{dd}.{MM}.{YY} {HH}:{mm}';
+    var dateParsed = new Date(dateString);
+    var prefixZero = function(value){
+        if(value < 10) {
+            return '0' + value;
+        }
+        return value;
+    };
+    return applyTemplate(dateTemplate, {
+        dd: prefixZero(dateParsed.getDay()),
+        MM: prefixZero(dateParsed.getMonth() + 1),
+        YY: dateParsed.getFullYear().toString().substr(-2),
+        HH: prefixZero(dateParsed.getHours()),
+        mm: prefixZero(dateParsed.getMinutes())
+    })
+};
 
 self.addEventListener('push', function(event) {
     if (!(self.Notification && self.Notification.permission === 'granted')) {
@@ -50,7 +76,7 @@ self.addEventListener('push', function(event) {
     self.registration.showNotification(
         pushData.notification.title,
         {
-            body: pushData.data.dateTime + pushData.notification.body,
+            body: getDateString(pushData.data.dateTime) + ' ' + pushData.notification.body,
             icon: pushData.notification.icon});
 
 });
