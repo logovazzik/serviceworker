3c0214bed8df491cafd1268b7399bfbe920472db
Vladimir <logovazzik@gmail.com>
2017-11-28 23:51:43 +0300

    test handler

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -46,8 +46,15 @@ var getDateString =  function(dateString){
 
 messaging.setBackgroundMessageHandler(function(payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    // Customize notification here
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
 
-    return Promise((res) => { res();})
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
 });
 
 // self.addEventListener('push', function(event) {
