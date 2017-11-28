c34bb6deab50cef669fd2e966c7eeead3fd235c4
Vladimir <logovazzik@gmail.com>
2017-11-29 00:44:46 +0300

    ###

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
 var config = {
     apiKey: "AIzaSyBXH1N-C-3Gi80tglAOQccdurbjukhu2Io",
