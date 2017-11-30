7b990dc942e3e08458ef2d9b172b10db5e5722ac
Vladimir <vshifrin@GDSpb0800-wf.lupus.griddynamics.net>
2017-11-30 20:03:49 +0300

    add helper

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
+importScripts('messaging-helper.js');
 
 var config = {
     apiKey: "AIzaSyBXH1N-C-3Gi80tglAOQccdurbjukhu2Io",
@@ -16,46 +17,11 @@ self.addEventListener('notificationclick', function(event) {
     event.notification.close();
 });
 
-var applyTemplate = function (template, replacements) {
-    return template.replace(/{(\w+)}/g, function (e, n) {
-        return undefined !== replacements[n] ? encodeURIComponent(replacements[n]) : "";
-    });
-};
-messaging.onMessage(function(payload) {
-    console.log('Message received from sw. ', payload);
-})
-
-var getDateString =  function(dateString){
-    if(!dateString){
-        return '';
-    }
-    var dateTemplate = '{dd}.{MM}.{YY} {HH}:{mm}';
-    var dateParsed = new Date(dateString);
-    var prefixZero = function(value){
-        if(value < 10) {
-            return '0' + value;
-        }
-        return value;
-    };
-    return applyTemplate(dateTemplate, {
-        dd: prefixZero(dateParsed.getDay()),
-        MM: prefixZero(dateParsed.getMonth() + 1),
-        YY: dateParsed.getFullYear().toString().substr(-2),
-        HH: prefixZero(dateParsed.getHours()),
-        mm: prefixZero(dateParsed.getMinutes())
-    })
-};
-
 
 messaging.setBackgroundMessageHandler(function(payload) {
       if (!(self.Notification && self.Notification.permission === 'granted')) {
          return;
       }
-
-    return self.registration.showNotification(
-        payload.data.title,
-         {
-             body: getDateString(payload.data.dateTime) + ' ' + payload.data.body,
-             icon: payload.data.icon
-        });
+    return showNotification(registration);
+    
 });
---
+++ messaging-helper.js
@@ -0,0 +1,40 @@
+showNotification = (function(){
+    var applyTemplate = function (template, replacements) {
+        return template.replace(/{(\w+)}/g, function (e, n) {
+            return undefined !== replacements[n] ? encodeURIComponent(replacements[n]) : "";
+        });
+    };
+    
+    var getDateString =  function(dateString){
+        if(!dateString){
+            return '';
+        }
+        
+        var dateTemplate = '{dd}.{MM}.{YY} {HH}:{mm}';
+        var dateParsed = new Date(dateString);
+        var prefixZero = function(value){
+            if(value < 10) {
+                return '0' + value;
+            }
+            return value;
+        };
+
+        return applyTemplate(dateTemplate, {
+            dd: prefixZero(dateParsed.getDay()),
+            MM: prefixZero(dateParsed.getMonth() + 1),
+            YY: dateParsed.getFullYear().toString().substr(-2),
+            HH: prefixZero(dateParsed.getHours()),
+            mm: prefixZero(dateParsed.getMinutes())
+        })
+    };
+    
+    return function(registration) {
+        return registration.showNotification(
+            payload.data.title,
+             {
+                 body: getDateString(payload.data.dateTime) + ' ' + payload.data.body,
+                 icon: payload.data.icon
+            });
+        
+    }
+})();
